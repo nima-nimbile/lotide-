7613a584d79dbb574ddf7af5ef9d0480196b597d
Nima <nima.mohamed@hotmail.com>
2023-02-06 20:04:42 -0800

    assert

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(9989, 9989, 9989) + ` Assertion Passed: ${actual} === ${expected}`);
+  }
+
+  else {
+    console.log(String.fromCodePoint(9940, 9940, 9940) + ` Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
