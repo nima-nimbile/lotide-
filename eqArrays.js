c2efe91917514470cf9362bb6bf7171dddfc6f7b
Nima <nima.mohamed@hotmail.com>
2023-02-07 21:56:09 -0800

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(9989, 9989, 9989) + ` Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(String.fromCodePoint(9940, 9940, 9940) + ` Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arra1, arra2) {
+  if (arra1.toString() === arra2.toString()) {
+    return true;
+  } else {
+    return false;
+  }
+};
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
