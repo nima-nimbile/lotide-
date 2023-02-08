fa9b5c7100e7c2c4ce5640b05ee3f0c2cdbf4b99
Nima <nima.mohamed@hotmail.com>
2023-02-08 09:01:49 -0800

    W1 d2

---
+++ assertArraysEqual.js
@@ -0,0 +1,10 @@
+const assertArraysEqual = function(array1, array2) {
+  if (JSON.stringify(array1) === JSON.stringify(array2)) {
+    return console.log(String.fromCodePoint(9989, 9989, 9989) + ` Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    return  console.log(String.fromCodePoint(9940, 9940, 9940) + ` Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
