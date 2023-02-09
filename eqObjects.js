215d30fa33d92b5b343e70a53fef1fcab711313f
Nima <nima.mohamed@hotmail.com>
2023-02-09 12:31:28 -0800

    edited

--- eqObjects.js
+++ eqObjects.js
@@ -31,7 +31,7 @@ const eqObjects = function(object1, object2) {
   }
   return true;
 };
-module.exports = eqObjects;
+
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject= { size: "medium", color: "red" };
