18aea26ae496c2421253df833c31998ab64d026f
Nima <nima.mohamed@hotmail.com>
2023-02-08 20:04:42 -0800

    W1 D3

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(9989, 9989, 9989) + ` Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(String.fromCodePoint(9940, 9940, 9940) + ` Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let result = {};
+  let newString = sentence.split(" ").join("");
+  for (const item of newString) {
+
+    if (result[item]) {
+
+      result[item] += 1;
+    } else {
+      result[item] = 1;
+    }
+  }
+  return result;
+};
+
+const result1 = countLetters("lighthouse in the house");
+assertEqual(result1["l"], 1);
+assertEqual(result1["i"], 2);
+assertEqual(result1["e"], 3);
