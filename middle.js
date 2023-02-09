365effb2c1edecebdfa2b527e861ccb60c02a50b
Nima <nima.mohamed@hotmail.com>
2023-02-08 16:13:21 -0800

    W1 D2

--- middle.js
+++ middle.js
@@ -5,23 +5,21 @@ const assertArraysEqual = function(array1, array2) {
     return  console.log(String.fromCodePoint(9940, 9940, 9940) + ` Assertion Failed: ${array1} !== ${array2}`);
   }
 };
-const middle = function (array) {
+const middle = function(array) {
   let middle = [];
   if (array.length <= 2) {
     return middle;
-  }
-  else if (array.length % 2 === 0) {
+  } else if (array.length % 2 === 0) {
     middle.push(array[Math.floor((array.length - 1) / 2)]);
     middle.push(array[Math.floor((array.length) / 2)]);
 
-  }
-  else {
-    middle.push(array[Math.floor((array.length) / 2)])
+  } else {
+    middle.push(array[Math.floor((array.length) / 2)]);
 
   }
   return middle;
 };
 // console.log(middle([1, 2, 3, 4, 5, 6]))
-assertArraysEqual(middle([1]), [] );
-assertArraysEqual(middle([1, 2, 3]), [2] );
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4] );
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
