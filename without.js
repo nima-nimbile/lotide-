f1d2e251af245854350e7c9dc0dbb41eae0e186d
Nima <nima.mohamed@hotmail.com>
2023-02-11 17:14:41 -0800

    Saturday work

--- without.js
+++ without.js
@@ -1,12 +1,25 @@
 
-const without = function(array, itemsToRemove) {
+const without = function (array, itemsToRemove) {
   let newArr = [];
   array.forEach(item => {
     if(!itemsToRemove.includes(item)){
       newArr.push(item);
     }});
-    
+  // for (let i = 0; i < array.length; i++) {
+  //   let allow = true;
+  //   for (let j = 0; j < itemsToRemove.length; j++) {
+  //     if (array[i] === itemsToRemove[j]) {
+  //       allow = false;
+  //     }
+  //   }
+  //   if (allow === true) {
+  //     newArr.push(array[i])
+  //   } 
+
+
+  // }
   return newArr;
 
 };
-console.log(without(["1", "2", "3"], [1, 2, "3"]));
+// console.log(without(["1", "2", "3"], [1, 2, "3"]));
+console.log(without(["1", "2", "3"], [1, "2", 3, 4]));
